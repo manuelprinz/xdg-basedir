3daabc625d5ffcf69fe32ce2e8c49b07785a6f78
Manuel Prinz <manuel@pinguinkiste.de>
2017-05-29 12:50:45 +0200

    Reformat according to the Google Java Style

--- src/main/java/de/pinguinkiste/xdg/basedir/BaseDir.java
+++ src/main/java/de/pinguinkiste/xdg/basedir/BaseDir.java
@@ -1,78 +1,86 @@
 package de.pinguinkiste.xdg.basedir;
 
+import static java.util.Collections.unmodifiableList;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
-import static java.util.Collections.unmodifiableList;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 
 public class BaseDir {
 
-    private final Environment environment;
+  private final Environment environment;
 
-    public BaseDir() {
-        this(new Environment());
-    }
+  public BaseDir() {
+    this(new Environment());
+  }
 
-    protected BaseDir(Environment environment) {
-        this.environment = environment;
-    }
+  protected BaseDir(Environment environment) {
+    this.environment = environment;
+  }
 
-    public String getUserDataDir() {
-        return defaultValueIfNotSet("XDG_DATA_HOME", ".local/share");
-    }
+  public String getUserDataDir() {
+    return defaultValueIfNotSet("XDG_DATA_HOME", ".local/share");
+  }
 
-    public String getUserConfigDir() {
-        return defaultValueIfNotSet("XDG_CONFIG_HOME", ".config");
-    }
+  public String getUserConfigDir() {
+    return defaultValueIfNotSet("XDG_CONFIG_HOME", ".config");
+  }
 
-    public String getUserCacheDir() {
-        return defaultValueIfNotSet("XDG_CACHE_HOME", ".cache");
-    }
+  public String getUserCacheDir() {
+    return defaultValueIfNotSet("XDG_CACHE_HOME", ".cache");
+  }
 
-    public List<String> getDataDirs() {
-        return defaultListIfNotSet("XDG_DATA_DIRS", "/usr/local/share:/usr/share");
-    }
+  public List<String> getDataDirs() {
+    return defaultListIfNotSet("XDG_DATA_DIRS", "/usr/local/share:/usr/share");
+  }
 
-    public List<String> getConfigDirs() {
-        return defaultListIfNotSet("XDG_CONFIG_DIRS", "/etc/xdg");
-    }
+  public List<String> getConfigDirs() {
+    return defaultListIfNotSet("XDG_CONFIG_DIRS", "/etc/xdg");
+  }
 
-    private String getValueOfHome() {
-        Optional<String> home = environment.valueOf("HOME");
-        if (!home.isPresent()) {
-            throw new IllegalStateException("The HOME environment variable is not set. This should not happen.");
-        }
-        return home.get();
+  @NotNull
+  private String getValueOfHome() {
+    Optional<String> home = environment.valueOf("HOME");
+    if (!home.isPresent()) {
+      throw new IllegalStateException(
+          "The HOME environment variable is not set. This should not happen.");
     }
+    return home.get();
+  }
 
-    private String defaultValueIfNotSet(String envVar, String postfix) {
-        String value = environment.valueOf(envVar).orElse("");
-        if (!value.equals("")) {
-            return value;
-        }
-        return getValueOfHome() + "/" + postfix;
+  private String defaultValueIfNotSet(String envVar, String postfix) {
+    String value = environment.valueOf(envVar).orElse("");
+    if (!value.equals("")) {
+      return value;
     }
+    return getValueOfHome() + "/" + postfix;
+  }
 
-    private List<String> defaultListIfNotSet(String envVar, String defaultList) {
-        Optional<String> dataDirsValue = environment.valueOf(envVar);
-        String dataDirs = defaultList;
-        if (dataDirsValue.isPresent() && isNotEmptyString(dataDirsValue.get())) {
-            dataDirs = dataDirsValue.get();
-        }
-        return listFromColonSeparatedString(dataDirs);
+  @NotNull
+  private List<String> defaultListIfNotSet(String envVar, String defaultList) {
+    Optional<String> dataDirsValue = environment.valueOf(envVar);
+    String dataDirs = defaultList;
+    if (dataDirsValue.isPresent() && isNotEmptyString(dataDirsValue.get())) {
+      dataDirs = dataDirsValue.get();
     }
+    return listFromColonSeparatedString(dataDirs);
+  }
 
-    private boolean isNotEmptyString(String string) {
-        return !string.equals("");
-    }
+  @Contract(pure = true)
+  private boolean isNotEmptyString(String string) {
+    return !string.equals("");
+  }
 
-    private List<String> listFromColonSeparatedString(String string) {
-        List<String> result = new ArrayList<>();
-        for (String element : string.split(":")) {
-            if (isNotEmptyString(element))
-                result.add(element);
-        }
-        return unmodifiableList(result);
+  @NotNull
+  private List<String> listFromColonSeparatedString(@NotNull String string) {
+    List<String> result = new ArrayList<>();
+    for (String element : string.split(":")) {
+      if (isNotEmptyString(element)) {
+        result.add(element);
+      }
     }
+    return unmodifiableList(result);
+  }
 }
--- src/main/java/de/pinguinkiste/xdg/basedir/Environment.java
+++ src/main/java/de/pinguinkiste/xdg/basedir/Environment.java
@@ -1,17 +1,15 @@
 package de.pinguinkiste.xdg.basedir;
 
-import java.util.Optional;
-
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNull.notNullValue;
 import static org.valid4j.Assertive.require;
 
-/**
- * Thin wrapper to retrieve environment values.
- */
+import java.util.Optional;
+
+/** Thin wrapper to retrieve environment values. */
 public class Environment {
-    public Optional<String> valueOf(String name) {
-        require(name, is(notNullValue()));
-        return Optional.ofNullable(System.getenv(name));
-    }
+  public Optional<String> valueOf(String name) {
+    require(name, is(notNullValue()));
+    return Optional.ofNullable(System.getenv(name));
+  }
 }
